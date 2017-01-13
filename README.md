0ab74726e5c4271286d3e55f3c21c1cfd25afb7e
Alicia Sedarski <alicia.sedarski@dominionenterprises.com>
2017-01-12 20:07:43 -0500

    Creating Listing Table and Defining Instructions for Updating SQL

--- README.md
+++ README.md
@@ -14,8 +14,16 @@ Built with:
   * jQuery
   * Composer
   * PHP SendMail
+  * Doctrine
+  * mySQL
 
 To run locally:
   * run `composer update`
   * run `php bin/console server:run`
   * Open <http://localhost:8000/>
+
+To update the database after making changes to Entity files:
+  * run `php bin/console doctrine:generate:entities AppBundle`
+  * run `php bin/console doctrine:schema:update --force`
+  * run `php bin/console doctrine:schema:validate` to verify database is up to date
+  * NOTE: run `php bin/console doctrine:schema:update --dump-sql` to see differences between schema and Entity
---
+++ src/AppBundle/Entity/Listing.php
@@ -0,0 +1,348 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="listing")
+ */
+class Listing
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text", length=3000)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer", length=4)
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $make;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $transmissionType;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $bodyStyle;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $options;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Listing
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Listing
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Listing
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Listing
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set year
+     *
+     * @param integer $year
+     *
+     * @return Listing
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return integer
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set make
+     *
+     * @param string $make
+     *
+     * @return Listing
+     */
+    public function setMake($make)
+    {
+        $this->make = $make;
+
+        return $this;
+    }
+
+    /**
+     * Get make
+     *
+     * @return string
+     */
+    public function getMake()
+    {
+        return $this->make;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return Listing
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Listing
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set transmissionType
+     *
+     * @param string $transmissionType
+     *
+     * @return Listing
+     */
+    public function setTransmissionType($transmissionType)
+    {
+        $this->transmissionType = $transmissionType;
+
+        return $this;
+    }
+
+    /**
+     * Get transmissionType
+     *
+     * @return string
+     */
+    public function getTransmissionType()
+    {
+        return $this->transmissionType;
+    }
+
+    /**
+     * Set bodyStyle
+     *
+     * @param string $bodyStyle
+     *
+     * @return Listing
+     */
+    public function setBodyStyle($bodyStyle)
+    {
+        $this->bodyStyle = $bodyStyle;
+
+        return $this;
+    }
+
+    /**
+     * Get bodyStyle
+     *
+     * @return string
+     */
+    public function getBodyStyle()
+    {
+        return $this->bodyStyle;
+    }
+
+    /**
+     * Set options
+     *
+     * @param array $options
+     *
+     * @return Listing
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+}
